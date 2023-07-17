21cd312bbc245c9669216c4188b20584bd0533f9
qwer2002hamd <136924700+qwer2002hamd@users.noreply.github.com>
2023-07-17 21:16:28 +0300

    Update config.py

--- config.py
+++ config.py
@@ -97,7 +97,7 @@ STREAM_IMG_URL = "https://te.legra.ph/file/c75a4d4a6e3a6f10cd98c.jpg"
 
 SOUNCLOUD_IMG_URL = "https://te.legra.ph/file/bb0ff85f2dd44070ea519.jpg"
 
-YOUTUBE_IMG_URL = "https://te.legra.ph/file/9de3eaa42476cb5e0ac16.jpg"
+YOUTUBE_IMG_URL = "https://te.legra.ph/file/03874b341976fa7352e2a.jpg"
 
 SPOTIFY_ARTIST_IMG_URL = "https://te.legra.ph/file/37d163a2f75e0d3b403d6.jpg"
 
