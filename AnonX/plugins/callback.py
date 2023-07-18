87d0978e20df61b538b20c44c840ea652348a45f
qwer2002hamd <136924700+qwer2002hamd@users.noreply.github.com>
2023-07-18 17:40:10 +0300

    Update callback.py

--- AnonX/plugins/callback.py
+++ AnonX/plugins/callback.py
@@ -29,6 +29,8 @@ from AnonX.utils.formatters import seconds_to_min
 from AnonX.utils.inline import (
     stream_markup,
     panel_markup_1,
+    panel_markup_2,
+    panel_markup_3,
     stream_markup_timer,
     telegram_markup,
     telegram_markup_timer,
@@ -86,6 +88,37 @@ async def del_back_playlist(client, CallbackQuery, _):
         checker[chat_id] = {}
     checker[chat_id][CallbackQuery.message.message_id] = True
 
+
+
+@app.on_callback_query(filters.regex("Pages") & ~BANNED_USERS)
+@languageCB
+async def del_back_playlist(client, CallbackQuery, _):
+    await CallbackQuery.answer()
+    callback_data = CallbackQuery.data.strip()
+    callback_request = callback_data.split(None, 1)[1]
+    state, pages, videoid, chat = callback_request.split("|")
+    chat_id = int(chat)
+    pages = int(pages)
+    if state == "Forw":
+        if pages == 0:
+            buttons = panel_markup_2(_, videoid, chat_id)
+        if pages == 2:
+            buttons = panel_markup_1(_, videoid, chat_id)
+        if pages == 1:
+            buttons = panel_markup_3(_, videoid, chat_id)
+    if state == "Back":
+        if pages == 2:
+            buttons = panel_markup_2(_, videoid, chat_id)
+        if pages == 1:
+            buttons = panel_markup_1(_, videoid, chat_id)
+        if pages == 0:
+            buttons = panel_markup_3(_, videoid, chat_id)
+    try:
+        await CallbackQuery.edit_message_reply_markup(
+            reply_markup=InlineKeyboardMarkup(buttons)
+        )
+    except:
+        return
 
 
 
