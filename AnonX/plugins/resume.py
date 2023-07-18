ec5a7d21af0405bb1477e65271d5a039e657c8e2
qwer2002hamd <136924700+qwer2002hamd@users.noreply.github.com>
2023-07-18 16:47:17 +0300

    Update resume.py

--- AnonX/plugins/resume.py
+++ AnonX/plugins/resume.py
@@ -30,3 +30,22 @@ async def resume_com(cli, message: Message, _, chat_id):
         _["admin_4"].format(message.from_user.first_name),
         reply_markup=close_keyboard
     )
+
+
+@app.on_message(
+    command(RESUME_COMMAND)
+    & filters.channel
+    & ~BANNED_USERS
+)
+@AdminRightsCheck
+async def resume_com(cli, message: Message, _, chat_id):
+    if not len(message.command) == 1:
+        return await message.reply_text(_["general_2"])
+    if await is_music_playing(chat_id):
+        return await message.reply_text(_["admin_3"])
+    await music_on(chat_id)
+    await Anon.resume_stream(chat_id)
+    await message.reply_text(
+        _["admin_4"].format(message.from_user.first_name),
+        reply_markup=close_keyboard
+    )
