3375e1ad28e94f27c0c5a5adee13b71898b3d773
qwer2002hamd <136924700+qwer2002hamd@users.noreply.github.com>
2023-07-18 17:45:25 +0300

    Update callback.py

--- AnonX/plugins/callback.py
+++ AnonX/plugins/callback.py
@@ -26,16 +26,11 @@ from AnonX.utils.database import (
 )
 from AnonX.utils.decorators.language import languageCB
 from AnonX.utils.formatters import seconds_to_min
-from AnonX.utils.inline import (
-    stream_markup,
-    panel_markup_1,
-    panel_markup_2,
-    panel_markup_3,
-    stream_markup_timer,
-    telegram_markup,
-    telegram_markup_timer,
-    close_keyboard,
-)
+from AnonX.utils.inline.play import (panel_markup_1,
+                                     panel_markup_2,
+                                     panel_markup_3,
+                                     stream_markup,
+                                     telegram_markup)
 from AnonX.utils.stream.autoclear import auto_clean
 from AnonX.utils.thumbnails import gen_thumb
 
