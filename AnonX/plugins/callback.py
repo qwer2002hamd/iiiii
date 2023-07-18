63ec79ae5d037f59135c1d583d560161315fcad1
qwer2002hamd <136924700+qwer2002hamd@users.noreply.github.com>
2023-07-18 23:22:30 +0300

    Update callback.py

--- AnonX/plugins/callback.py
+++ AnonX/plugins/callback.py
@@ -486,15 +486,9 @@ async def markup_timer():
                     _ = get_string("en")
                 try:
                     buttons = (
-                        stream_markup_timer(
+                        telegram_markup_timer(
                             _,
-                            chat_id,
-                            seconds_to_min(playing[0]["played"]),
-                            playing[0]["dur"],
-                        )
-                        if markup == "stream"
-                        else telegram_markup_timer(
-                            _,
+                            playing[0]["vidid"],
                             chat_id,
                             seconds_to_min(playing[0]["played"]),
                             playing[0]["dur"],
