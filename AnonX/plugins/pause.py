f25666086d7b020f590dd02fa22106ae49336980
qwer2002hamd <136924700+qwer2002hamd@users.noreply.github.com>
2023-07-18 16:46:15 +0300

    Update pause.py

--- AnonX/plugins/pause.py
+++ AnonX/plugins/pause.py
@@ -30,3 +30,22 @@ async def pause_admin(cli, message: Message, _, chat_id):
         _["admin_2"].format(message.from_user.first_name),
         reply_markup=close_keyboard
     )
+
+
+@app.on_message(
+    command(PAUSE_COMMAND)
+    & filters.channel
+    & ~BANNED_USERS
+)
+@AdminRightsCheck
+async def pause_admin(cli, message: Message, _, chat_id):
+    if not len(message.command) == 1:
+        return await message.reply_text(_["general_2"])
+    if not await is_music_playing(chat_id):
+        return await message.reply_text(_["admin_1"])
+    await music_off(chat_id)
+    await Anon.pause_stream(chat_id)
+    await message.reply_text(
+        _["admin_2"].format(message.from_user.first_name),
+        reply_markup=close_keyboard
+    )
