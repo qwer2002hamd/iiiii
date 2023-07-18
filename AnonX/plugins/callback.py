b312c22fd4387a5242972faa2941cc4ba7169089
qwer2002hamd <136924700+qwer2002hamd@users.noreply.github.com>
2023-07-18 22:37:41 +0300

    Update callback.py

--- AnonX/plugins/callback.py
+++ AnonX/plugins/callback.py
@@ -52,7 +52,7 @@ async def markup_panel(client, CallbackQuery: CallbackQuery, _):
     callback_request = callback_data.split(None, 1)[1]
     videoid, chat_id = callback_request.split("|")
     chat_id = CallbackQuery.message.chat.id
-    buttons = panel_markup_1(_, videoid, chat_id, user_id, query, query_type, channel, fplay)
+    buttons = panel_markup_1(_, videoid, chat_id)
     try:
         await CallbackQuery.edit_message_reply_markup(
             reply_markup=InlineKeyboardMarkup(buttons)
@@ -101,18 +101,18 @@ async def del_back_playlist(client, CallbackQuery, _):
     pages = int(pages)
     if state == "Forw":
         if pages == 0:
-            buttons = panel_markup_2(_, videoid, chat_id, user_id, query, query_type, channel, fplay)
+            buttons = panel_markup_2(_, videoid, chat_id)
         if pages == 2:
-            buttons = panel_markup_1(_, videoid, chat_id, user_id, query, query_type, channel, fplay)
+            buttons = panel_markup_1(_, videoid, chat_id)
         if pages == 1:
-            buttons = panel_markup_3(_, videoid, chat_id, user_id, query, query_type, channel, fplay)
+            buttons = panel_markup_3(_, videoid, chat_id)
     if state == "Back":
         if pages == 2:
-            buttons = panel_markup_2(_, videoid, chat_id, user_id, query, query_type, channel, fplay)
+            buttons = panel_markup_2(_, videoid, chat_id)
         if pages == 1:
-            buttons = panel_markup_1(_, videoid, chat_id, user_id, query, query_type, channel, fplay)
+            buttons = panel_markup_1(_, videoid, chat_id)
         if pages == 0:
-            buttons = panel_markup_3(_, videoid, chat_id, user_id, query, query_type, channel, fplay)
+            buttons = panel_markup_3(_, videoid, chat_id)
     try:
         await CallbackQuery.edit_message_reply_markup(
             reply_markup=InlineKeyboardMarkup(buttons)
