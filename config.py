ab58fb4ed013ef3da128f773a9ed97591392395d
qwer2002hamd <136924700+qwer2002hamd@users.noreply.github.com>
2023-07-19 15:18:46 +0300

    Update config.py

--- config.py
+++ config.py
@@ -86,13 +86,13 @@ PING_IMG_URL = getenv(
 
 PLAYLIST_IMG_URL = "https://te.legra.ph/file/c75a4d4a6e3a6f10cd98c.jpg"
 
-GLOBAL_IMG_URL = "https://te.legra.ph/file/e906c2def5afe8a9b9120.jpg"
+GLOBAL_IMG_URL = "https://te.legra.ph/file/c16fcfcda43974fd29cb8.jpg"
 
 STATS_IMG_URL = "https://te.legra.ph/file/c75a4d4a6e3a6f10cd98c.jpg"
 
-TELEGRAM_AUDIO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
+TELEGRAM_AUDIO_URL = "https://te.legra.ph/file/c16fcfcda43974fd29cb8.jpg"
 
-TELEGRAM_VIDEO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
+TELEGRAM_VIDEO_URL = "https://te.legra.ph/file/c16fcfcda43974fd29cb8.jpg"
 
 STREAM_IMG_URL = "https://te.legra.ph/file/c75a4d4a6e3a6f10cd98c.jpg"
 
