eb6eebcfb46cfbbb7d47e08f38e1c95b47d9ba3f
qwer2002hamd <136924700+qwer2002hamd@users.noreply.github.com>
2023-07-19 03:30:16 +0300

    Update الغاء الكتم.py

--- AnonX/plugins/الغاء الكتم.py
+++ AnonX/plugins/الغاء الكتم.py
@@ -1,15 +1,15 @@
 from pyrogram import filters
 from pyrogram.types import Message
-
+from strings.filters import command
 from config import BANNED_USERS
 from strings import get_command
 from AnonX import app
 from AnonX.core.call import Anon
-from AnonX.utils.database import is_music_playing, music_off
+from AnonX.utils.database import is_music_playing, music_on
 from AnonX.utils.decorators import AdminRightsCheck
 from AnonX.utils.decorators.adminss import AdminRightsCheckk
 from AnonX.utils.inline.play import close_keyboard
-from strings.filters import command
+
 # Commands
 UNMUTE_COMMAND = get_command("UNMUTE_COMMAND")
 
@@ -20,15 +20,15 @@ UNMUTE_COMMAND = get_command("UNMUTE_COMMAND")
     & ~BANNED_USERS
 )
 @AdminRightsCheck
-async def resume_admin(cli, message: Message, _, chat_id):
+async def resume_com(cli, message: Message, _, chat_id):
     if not len(message.command) == 1:
         return await message.reply_text(_["general_6"])
-    if not await is_music_playing(chat_id):
-        return await message.reply_text(_["admin_7"])
+    if await is_music_playing(chat_id):
+        return await message.reply_text(_["admin_8"])
     await music_on(chat_id)
     await Anon.resume_stream(chat_id)
     await message.reply_text(
-        _["admin_8"].format(message.from_user.first_name),
+        _["admin_7"].format(message.from_user.first_name),
         reply_markup=close_keyboard
     )
 
@@ -39,14 +39,14 @@ async def resume_admin(cli, message: Message, _, chat_id):
     & ~BANNED_USERS
 )
 @AdminRightsCheck
-async def resume_admin(cli, message: Message, _, chat_id):
+async def resume_com(cli, message: Message, _, chat_id):
     if not len(message.command) == 1:
         return await message.reply_text(_["general_6"])
-    if not await is_music_playing(chat_id):
-        return await message.reply_text(_["admin_7"])
+    if await is_music_playing(chat_id):
+        return await message.reply_text(_["admin_8"])
     await music_on(chat_id)
     await Anon.resume_stream(chat_id)
     await message.reply_text(
-        _["admin_8"].format(message.from_user.first_name),
+        _["admin_7"].format(message.from_user.first_name),
         reply_markup=close_keyboard
     )
