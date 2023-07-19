e292f9a6833056ae8f20e821936297c2135d9d6e
qwer2002hamd <136924700+qwer2002hamd@users.noreply.github.com>
2023-07-19 03:33:45 +0300

    Update الغاء الكتم.py

--- AnonX/plugins/الغاء الكتم.py
+++ AnonX/plugins/الغاء الكتم.py
@@ -24,11 +24,11 @@ async def resume_com(cli, message: Message, _, chat_id):
     if not len(message.command) == 1:
         return await message.reply_text(_["general_6"])
     if await is_music_playing(chat_id):
-        return await message.reply_text(_["admin_8"])
+        return await message.reply_text(_["admin_7"])
     await music_on(chat_id)
     await Anon.resume_stream(chat_id)
     await message.reply_text(
-        _["admin_7"].format(message.from_user.first_name),
+        _["admin_8"].format(message.from_user.first_name),
         reply_markup=close_keyboard
     )
 
@@ -43,10 +43,10 @@ async def resume_com(cli, message: Message, _, chat_id):
     if not len(message.command) == 1:
         return await message.reply_text(_["general_6"])
     if await is_music_playing(chat_id):
-        return await message.reply_text(_["admin_8"])
+        return await message.reply_text(_["admin_7"])
     await music_on(chat_id)
     await Anon.resume_stream(chat_id)
     await message.reply_text(
-        _["admin_7"].format(message.from_user.first_name),
+        _["admin_8"].format(message.from_user.first_name),
         reply_markup=close_keyboard
     )
