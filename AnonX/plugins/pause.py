e08a2d6378364615daddf8182e67b2855a0127dd
qwer2002hamd <136924700+qwer2002hamd@users.noreply.github.com>
2023-07-19 14:36:57 +0300

    Update pause.py

--- AnonX/plugins/pause.py
+++ AnonX/plugins/pause.py
@@ -5,7 +5,7 @@ from config import BANNED_USERS
 from strings import get_command
 from AnonX import app
 from AnonX.core.call import Anon
-from AnonX.utils.database import is_music_playing, music_off
+from AnonX.utils.database import set_loop
 from AnonX.utils.decorators import AdminRightsCheck
 from AnonX.utils.decorators.adminss import AdminRightsCheckk
 from AnonX.utils.inline.play import close_keyboard
@@ -20,33 +20,32 @@ PAUSE_COMMAND = get_command("PAUSE_COMMAND")
     & ~BANNED_USERS
 )
 @AdminRightsCheck
-async def pause_admin(cli, message: Message, _, chat_id):
+async def pause_music(cli, message: Message, _, chat_id):
     if not len(message.command) == 1:
         return await message.reply_text(_["general_2"])
     if not await is_music_playing(chat_id):
         return await message.reply_text(_["admin_1"])
-    await music_off(chat_id)
     await Anon.pause_stream(chat_id)
+    await music_off(chat_id, 0)
     await message.reply_text(
         _["admin_2"].format(message.from_user.first_name),
-        reply_markup=close_keyboard
+        reply_markup=close_keyboard,
     )
-
 
 @app.on_message(
     command(PAUSE_COMMAND)
     & filters.channel
     & ~BANNED_USERS
 )
-@AdminRightsCheck
-async def pause_admin(cli, message: Message, _, chat_id):
+@AdminRightsCheckk
+async def pause_music(cli, message: Message, _, chat_id):
     if not len(message.command) == 1:
         return await message.reply_text(_["general_2"])
     if not await is_music_playing(chat_id):
         return await message.reply_text(_["admin_1"])
-    await music_off(chat_id)
     await Anon.pause_stream(chat_id)
+    await music_off(chat_id, 0)
     await message.reply_text(
         _["admin_2"].format(message.from_user.first_name),
-        reply_markup=close_keyboard
+        reply_markup=close_keyboard,
     )
